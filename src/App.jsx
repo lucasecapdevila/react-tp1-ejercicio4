e42c870a7a0539e6629f78bad95ab1ffe02896b2
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-04 16:26:47 -0300

    se agrega footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import { Container } from 'react-bootstrap'
 import './App.css'
+import Footer from '../components/Footer'
 
 function App() {
 
   return (
     <>
-      <h1>Hola mundo !</h1>
+      <Container className='my-4 mainPage'>
+        <h1 className='display-1 text-center'>Lista de Tareas</h1>
+      </Container>
+
+      <Footer />
     </>
   )
 }
