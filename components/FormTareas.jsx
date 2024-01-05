18544f5466110f8ccc440d91b68df33448d53d63
Lucas Capdevila <lcapdevila60@gmail.com>
2024-01-05 01:09:05 -0300

    se agrega useState de tarea y lista de tareas

--- components/FormTareas.jsx
+++ components/FormTareas.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import ListaTareas from './ListaTareas';
 
 const FormTareas = () => {
+  const [tarea, setTarea] = useState('')
+  const [tareas, setTareas] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setTareas([...tareas, tarea])
+    setTarea('')
+  }
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="inputTarea">
           <Form.Control
             size='lg'
             type="text"
             minLength={5}
             maxLength={50}
+            value={tarea}
             placeholder="Ingrese sus tareas"
           />
           <Button variant='success' className='mx-2 px-4' type='submit'>Agregar</Button>
